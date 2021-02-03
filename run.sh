719d92cbeb6bbc1f95741879e1fb2fab509aa124
S. CM <33370667+phisanti@users.noreply.github.com>
2021-02-03 10:17:00 +0000

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Entrypoint for gradient
+
+# Source: https://gist.github.com/earthgecko/3089509
+# Generates a random alphanumeric string of length 48 (like a jupyter notebook token i.e. c8de56fa4deed24899803e93c227592aef6538f93025fe01)
+if [ -z "$JUPYTER_TOKEN" ]; then
+    JUPYTER_TOKEN=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 48 | head -n 1)
+fi
+
+# Note: print mocked jupyter token so that we can run this container as if it is a notebook within Gradient V1
+echo "http://localhost:8888/?token=${JUPYTER_TOKEN}"
+echo "http://localhost:8888/\?token\=${JUPYTER_TOKEN}"
+
+PASSWORD=${JUPYTER_TOKEN} /usr/bin/code-server --bind-addr "0.0.0.0:8888" .
